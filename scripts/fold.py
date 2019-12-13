bdddecb0591348dbe91602be2a72e0aea9264ece
Anthony Canino <anthony.canino1@gmail.com>
2019-12-13 17:49:37 +0000

    Latest edits.

--- scripts/fold.py
+++ scripts/fold.py
@@ -8,28 +8,26 @@ usage = "usage: %prog [options]"
 parser = OptionParser(usage=usage)
 parser.add_option('-d', '--dir', dest='working_dir', default='.', help='use DIR as working output directory', metavar='DIR') 
 parser.add_option('-p', '--preserve', dest='preserve', action='store_true', help='preserve process trace files (do not delete)')
+parser.add_option('-t', '--trace-name', dest='tracename', default='lzload.trace', help='use either lzload.trace or lztrace.trace')
 
 (options, args) = parser.parse_args()
 
-tracename = "lzload.trace"
-if "LZLOAD_TRACE" in os.environ:
-    tracename = os.environ["LZLOAD_TRACE"]
-
-files = glob.glob("{}/{}.*".format(options.working_dir, tracename))
+files = glob.glob("{}/{}.*".format(options.working_dir, options.tracename))
 used = set()
 for fn in files:
     print("merging {}".format(fn))
     with open(fn, "r") as f:
-        iused = set(f.readlines()[0].split())
-        if "nodep" not in iused:
-            used.update(iused)
+        for l in f.readlines():
+            used.add(l.strip())
     if not options.preserve:
         os.remove(fn)
 
-with open("{}/{}".format(options.working_dir, tracename), "a") as f:
+with open("{}/{}".format(options.working_dir, options.tracename), "a") as f:
+    sep = " " if options.tracename == "lzload.trace" else "\n"
     if len(used) > 0:
         for d in used:
-            f.write(d + " ")
+            f.write(d + sep)
         f.write("\n")
     else:
-        f.write("nodep\n")
+        if options.tracename == "lzload.trace":
+            f.write("nodep\n")
