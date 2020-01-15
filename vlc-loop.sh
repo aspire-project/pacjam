dfc720b5ceaa8565bce44de56014832d00cb1257
Anthony Canino <anthony.canino1@gmail.com>
2020-01-15 12:59:28 +0000

    Because I keep accidently deleting this.

---
+++ vlc-loop.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+export LZLOAD_LIB=/home/aspire/dep-trace/runtime.txt
+export LZLOAD_MODE="STRICT"
+
+# Path points to the real libraries
+export LZ_LIBRARY_PATH="src-out/vlc/mod-lib:/lib/x86_64-linux-gnu:/usr/lib/x86_64-linux-gnu/:/usr/lib/x86_64-linux-gnu/vlc:/usr/lib/x86_64-linux-gnu/pulseaudio"
+
+# Path points to our lzload and fake libraries
+
+while true; do
+	export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:src-out/vlc/lib"
+
+	vlc vlc_formats/avi_1.avi
+
+	if [ $? -eq 0 ]; then
+		echo "done"
+		exit
+	fi
+
+	./scripts/fold.py -d . -t lzload.trace
+	cat lzload.trace >> tmp.lzload.trace
+
+	./dep-src.py -d src-out/vlc -p $HOME/var/lib/lzload/symbol-out/packages.txt -i lzload.trace
+done
